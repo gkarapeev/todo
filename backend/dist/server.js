ec9502643154e702a87fbefa36004948fc86f454
Georgi K <gk@Georgis-MacBook-Air.local>
2022-10-30 00:56:31 +0300

    feat: separate fe & be projects

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -6,7 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = require("path");
 const app = (0, express_1.default)();
+app.use(express_1.default.static((0, path_1.join)(__dirname, '../../frontend/dist')));
 app.get('/', (req, res) => {
-    res.sendFile((0, path_1.join)(__dirname, '../dist/index.html'));
+    res.sendFile((0, path_1.join)(__dirname, '../../frontend/dist/index.html'));
 });
 app.listen(3000, () => console.log('Sverver running on port 3000!'));
--- backend/server.ts
+++ backend/server.ts
@@ -3,8 +3,10 @@ import { join } from "path";
 
 const app: Application = express();
 
+app.use(express.static(join(__dirname, '../../frontend/dist')))
+
 app.get('/', (req: Request, res: Response) => {
-    res.sendFile(join(__dirname, '../dist/index.html'));
+    res.sendFile(join(__dirname, '../../frontend/dist/index.html'));
 });
 
 app.listen(3000, () => console.log('Sverver running on port 3000!'));
